30339eca5c0d5d78e11485a9aa689b4e663c0505
jopa <ignatternyev54@gmail.com>
2021-02-10 00:46:04 +0300

    gfbfbgfbgf

--- Python/PythonHandler.py
+++ Python/PythonHandler.py
@@ -2,6 +2,7 @@ import clr
 import sys
 import System
 from System import Action
+from pdf2image import convert_from_path
 def CallBack(__x__,__y__):
     try:
         exec(__y__)
@@ -12,4 +13,11 @@ def CallBack(__x__,__y__):
         else: 
             __x__.Invoke("Python: " + ex.message)
 
-CallBack(act,text)  
+def PdfToImage(__x__):
+    pages = convert_from_path('pdf_file', 500)
+    for page in pages:
+        page.save('out.jpg', 'JPEG')
+if act != None:
+    CallBack(act,text)  
+
+
